cdf50c4b84b64043433433aec29fef9bf5bd9cb3
Yinka Akindele <akindeleyinka1005@gmail.com>
2024-08-12 18:06:07 +0100

    Fixing file path issue

--- web_app/data_loader.py
+++ web_app/data_loader.py
@@ -4,14 +4,13 @@ import streamlit as st
 @st.cache_data
 def load_data():
     # Load the Excel file
-    file_path = "Student_Data.xlsx"
     
     # Load the individual sheets into dataframes
-    Academic_Performance = pd.read_excel(file_path, sheet_name='Academic_Performance')
-    Biodata = pd.read_excel(file_path, sheet_name='Biodata')
-    First_and_Last_Result = pd.read_excel(file_path, sheet_name='First_and_Last_Result')
-    Registration = pd.read_excel(file_path, sheet_name='Registration')
-    Result_Sheet = pd.read_excel(file_path, sheet_name='Result_sheet')
+    Academic_Performance = pd.read_excel("Student_Data.xlsx", sheet_name='Academic_Performance')
+    Biodata = pd.read_excel("Student_Data.xlsx", sheet_name='Biodata')
+    First_and_Last_Result = pd.read_excel("Student_Data.xlsx", sheet_name='First_and_Last_Result')
+    Registration = pd.read_excel("Student_Data.xlsx", sheet_name='Registration')
+    Result_Sheet = pd.read_excel("Student_Data.xlsx", sheet_name='Result_sheet')
     
     return {
         "Academic_Performance": Academic_Performance,
